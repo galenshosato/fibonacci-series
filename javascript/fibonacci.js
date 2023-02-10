36ef23c7a8b0e62309d76abe350d4c11bb6bab02
Galen Sho Sato <galen.sato@gmail.com>
2023-02-10 16:14:58 -0500

    Solution arrived at

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,6 +1,49 @@
+// 0 1 1 2 3 5 8 13...
+
+const cache = {}
+
+
 function fibonacci(num) {
-  // type your code here
+  if (cache[num] !== undefined) {
+    console.log('cache hit')
+    return cache[num]
+  }
+  if (num === 0 ) {
+    return 0
+  }
+
+  if (num === 1) {
+    return 1
+  }
+  const prev = fibonacci(num -1)
+  cache[num-1] = prev
+  const prevPrev = fibonacci(num - 2)
+  cache[num-2] = prevPrev
+
+  return prev + prevPrev
+
 }
+
+
+// function fibonacci(num) {
+//   if (num === 0) {
+//     return 0
+//   }
+
+//   if (num === 1) {
+//     return 1
+//   }
+
+//   const seq = [0,1]
+//   while (seq.length <= num ) {
+//     const last = seq[seq.length-1]
+//     const secondLast = seq[seq.length-2]
+//     const newVal = last + secondLast
+//     seq.push(newVal)
+//   }
+
+//   return seq[seq.length-1]
+// }
 
 if (require.main === module) {
   // add your own tests in here
